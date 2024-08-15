caa7f64254b5406a2073ccbb38220ad114a6f2ff
Evelyn Montgomery <evelynmontgomery@Evelyns-Air.lan>
2024-08-15 11:07:28 -0700

    added color changing functionality to tempDisplay

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+//color changing functionality
+function whatColor(currentTemp) {
+    if (currentTemp >= 80){
+        document.getElementById("tempDisplay").style.color = "red"
+    } else if (currentTemp >= 70 && currentTemp <= 79) {
+        document.getElementById("tempDisplay").style.color = "orange"
+    } else if (currentTemp >=60 && currentTemp <= 69) {
+        document.getElementById("tempDisplay").style.color = "yellow"
+    } else if (currentTemp >= 50 && currentTemp <= 59) {
+        document.getElementById("tempDisplay").style.color = "green"
+    } else if (currentTemp <= 49){
+        document.getElementById("tempDisplay").style.color = "teal"
+    }
+}
+
 //upButton functionality
 let upButton = document.querySelector("upButton")
 
@@ -6,9 +21,10 @@ upButton.addEventListener("click", increaseByOne())
 function increaseByOne() {
     let stringNumber = document.getElementById('tempDisplay').innerHTML;
 
-let currentTemp = parseInt(stringNumber)
+    let currentTemp = parseInt(stringNumber)
     currentTemp += 1
     document.getElementById('tempDisplay').innerHTML= currentTemp
+    whatColor(currentTemp)
 }
 
 
@@ -23,4 +39,5 @@ function decreaseByOne() {
     let currentTemp = parseInt(stringNumber)
     currentTemp -= 1
     document.getElementById('tempDisplay').innerHTML = currentTemp
+    whatColor(currentTemp)
 }
